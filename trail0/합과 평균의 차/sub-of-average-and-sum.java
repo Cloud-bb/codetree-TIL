19802102bbecb4ace0b4d193dab1cfb6c615e370
cloudyB <157672950+Cloud-bb@users.noreply.github.com>
2026-06-17 17:52:55 +0900

    [Passed] 합과 평균의 차 (129ms, 10MB)

--- trail0/합과 평균의 차/sub-of-average-and-sum.java
+++ trail0/합과 평균의 차/sub-of-average-and-sum.java
@@ -1,18 +1,13 @@
+
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
+        int a = sc.nextInt();        
         int b = sc.nextInt();
         int c = sc.nextInt();
-
-        int hap = a+b+c;
-        int avg = hap/3;
-        System.out.println(hap);    
-        System.out.println(avg);
-        System.out.println(hap-avg);
-
-
+        int sum = a + b + c;
+        System.out.printf("%d\n%d\n%d", sum, sum / 3, sum - sum/3);
     }
 }
