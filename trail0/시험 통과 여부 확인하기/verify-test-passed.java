ddd9c2b91356af2c7add6085e07474efa2282657
cloudyB <157672950+Cloud-bb@users.noreply.github.com>
2026-06-18 17:32:20 +0900

    [Passed] 시험 통과 여부 확인하기 (137ms, 10MB)

--- trail0/시험 통과 여부 확인하기/verify-test-passed.java
+++ trail0/시험 통과 여부 확인하기/verify-test-passed.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(system.in);
+        Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         if(N>=80){
             System.out.println("pass");
