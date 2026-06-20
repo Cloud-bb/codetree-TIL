316b0d2535577a008ada6ff6d2283b2aceb79bee
cloudyB <157672950+Cloud-bb@users.noreply.github.com>
2026-06-20 18:21:47 +0900

    [Passed] 차례로 출력 (90ms, 9MB)

--- trail0/차례로 출력/print-in-order.java
+++ trail0/차례로 출력/print-in-order.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Scanner sc = new Scanner(System.in);
         for(int i = 5;i<=17;i++){
             System.out.print(i+" ");
         }
